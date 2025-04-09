1f911270745e9b68999c553e806592da933e68cb
OChalupe <chalupe@gmail.com>
2025-04-09 16:58:53 -0300

    testes

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+    String nomeum = "Eduardo";
+    String nomeDois = "Eduardo";
+       
+    int numero1 = 1;
+    int numero2 = 2;
+
+    boolean simNao = numero1 == numero2;
+
+    if(numero1 < numero2) {
+        System.out.println("a nossa condição é verdadeira");
+    }
+
+       
+        
+    }
+}
